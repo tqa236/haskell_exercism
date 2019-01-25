c42a59c421386ee728be224d8bb366336e1da3a2
TQA <trinhquocanh94@gmail.com>
2019-01-25 11:37:37 +0100

    Solve leap exercise.

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,4 +1,10 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year =
+  if mod year 4 == 0
+  then
+    if mod year 100 == 0 && mod year 400 /= 0
+    then False
+    else True
+  else False
