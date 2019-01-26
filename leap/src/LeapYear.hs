1f1c715bc5363a57999bd436ad3f4fd0005c062f
TQA <trinhquocanh94@gmail.com>
2019-01-26 21:28:43 +0100

    Improve leap solution.

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,10 +1,11 @@
 module LeapYear (isLeapYear) where
 
+divided :: Integer -> Integer -> Bool
+divided year n =  year `rem` n == 0
+
 isLeapYear :: Integer -> Bool
-isLeapYear year =
-  if mod year 4 == 0
-  then
-    if mod year 100 == 0 && mod year 400 /= 0
-    then False
-    else True
-  else False
+isLeapYear year
+  | divided year 4 = case divided year 100 of
+    True  -> divided year 400
+    False -> True
+  | otherwise = False
