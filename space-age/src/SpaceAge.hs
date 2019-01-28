36397918bdea276d018ddf0a638e4e79982848ee
TQA <trinhquocanh94@gmail.com>
2019-01-28 19:44:34 +0100

    Download new exercises.

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -10,18 +10,14 @@ data Planet = Mercury
             | Neptune
             deriving Eq
 
-year_length :: Planet -> Float
-year_length planet
-  | planet == Mercury = 0.2408467
-  | planet == Venus = 0.61519726
-  | planet == Earth = 1.0
-  | planet == Mars = 1.8808158
-  | planet == Jupiter = 11.862615
-  | planet == Saturn = 29.447498
-  | planet == Uranus = 84.016846
-  | planet == Neptune = 164.79132
-  | otherwise = error "Not a planet!"
-
-
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = seconds/(31557600 * year_length planet)
+ageOn planet seconds = seconds/(31557600 * year_length)
+  where year_length = case planet of
+                        Mercury -> 0.2408467
+                        Venus   -> 0.61519726
+                        Earth   -> 1.0
+                        Mars    -> 1.8808158
+                        Jupiter -> 11.862615
+                        Saturn  -> 29.447498
+                        Uranus  -> 84.016846
+                        Neptune -> 164.79132
