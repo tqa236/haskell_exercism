69aa377d20a7a694326b3fd704c96015fa11947d
TQA <trinhquocanh94@gmail.com>
2019-01-29 22:18:16 +0100

    Solve bob exercise.

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -6,10 +6,11 @@ import           Data.Char
 responseFor :: String -> String
 responseFor xs = case (yell, question, no_alpha, empty) of
     (_, _, _,  True)    -> "Fine. Be that way!"
+    (_, False, True, _) -> "Whatever."
+
     (False, True, _, _) -> "Sure."
     (_, True, True,  _) -> "Sure."
     (True, True, _, _)  -> "Calm down, I know what I'm doing!"
-    (_, False, True, _) -> "Whatever."
     (True, False, _, _) -> "Whoa, chill out!"
     _                   ->  "Whatever."
     where yell = filter (isAlpha) xs == filter (isAlpha) (map Data.Char.toUpper xs)
