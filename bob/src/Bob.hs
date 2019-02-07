dbd9581fc20b7d28222398d6b90985c58e6b96f8
TQA <trinhquocanh94@gmail.com>
2019-02-07 08:03:03 +0100

    Improve bob solution

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -5,15 +5,12 @@ import           Data.Char
 responseFor :: String -> String
 responseFor xs
     | iSEmpty = "Fine. Be that way!"
-    | isQuestion = checkLetter
-    | noLetter = "Whatever."
-    | noLowercaseLetter = "Whoa, chill out!"
+    | isQuestion = checkYelling
+    | isYelling = "Whoa, chill out!"
     | otherwise = "Whatever."
     where iSEmpty = null (filter (not . isSpace) xs)
-          noLowercaseLetter = null (filter (isLower) xs) && not null (filter (isUpper) xs)
+          isYelling = null (filter (isLower) xs) && not (null (filter (isUpper) xs))
           isQuestion = last ( filter (/= ' ') xs)  == '?'
-          noLetter = null (filter (isAlpha) xs)
-          checkLetter
-            | noLetter = "Sure."
-            | noLowercaseLetter = "Calm down, I know what I'm doing!"
+          checkYelling
+            | isYelling = "Calm down, I know what I'm doing!"
             | otherwise = "Sure."
