c521dd449d859117d9a06fd3c461966d5e92cd6f
TQA <trinhquocanh94@gmail.com>
2019-02-08 21:45:46 +0100

    Improve bob solution and download new exercises

---
+++ pangram/src/Pangram.hs
@@ -0,0 +1,9 @@
+module Pangram (isPangram) where
+
+import           Data.Char
+import qualified Data.Set  as Set
+
+isPangram :: String -> Bool
+isPangram text
+    | Set.size (Set.fromList (map toLower (filter (isAlpha) text))) < 26 = False
+    | otherwise = True
