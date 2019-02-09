6c07176882a35abce8c50f3042e4bbb17ae180f0
TQA <trinhquocanh94@gmail.com>
2019-02-09 01:57:05 +0100

    Solve secret handshake

--- secret-handshake/src/SecretHandshake.hs
+++ secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,11 @@
 module SecretHandshake (handshake) where
 
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake n
+    | n >= 32 = handshake $ n `mod` 32
+    | n >= 16 = reverse $ handshake $ n - 16
+    | n >= 8 = handshake (n - 8) ++ ["jump"]
+    | n >= 4 = handshake (n - 4) ++ ["close your eyes"]
+    | n >= 2 = handshake (n - 2) ++ ["double blink"]
+    | n == 1 = ["wink"]
+    | otherwise = []
