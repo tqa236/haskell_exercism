be72624a3fbd7c26ae3f7910099828ea28c5ac18
TQA <trinhquocanh94@gmail.com>
2019-02-11 17:43:11 +0100

    Improve collatz

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,7 +3,10 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz number
     | number <= 0 = Nothing
-    | number == 1 = Just 0
-    | number `mod` 2 == 0 = maybeAdd $ collatz (number `div` 2)
-    | otherwise = maybeAdd $ collatz (number * 3 + 1)
-    where maybeAdd num = sum <$> sequence [Just 1, num]
+    | otherwise = Just $ toInteger
+                       $ length
+                       $ takeWhile (> 1)
+                       $ iterate nextCollatz number
+    where nextCollatz x
+            | even x = x `quot` 2
+            | otherwise = x * 3 + 1
--- word-count/src/WordCount.hs
+++ word-count/src/WordCount.hs
@@ -2,6 +2,9 @@ module WordCount (wordCount) where
 
 import           Data.Char
 import           Data.List
+import           Data.Text (pack, replace, unpack)
 
 wordCount :: String -> [(String, Int)]
-wordCount xs = map (\w -> (head w, length w)) $ group $ sort $ words xs
+wordCount xs = map (\w -> (head w, length w)) $ group $ sort $ words noQuotation
+    where noQuotation =  unpack . replace "\' " " " . pack
+          sentence = map (\c -> if not (isAlphaNum c || (c == '\'')) then ' ' else toLower c) xs
