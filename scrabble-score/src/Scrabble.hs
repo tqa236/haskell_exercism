0e509af3e64e219674ff947451ec11ca61c55fa8
TQA <trinhquocanh94@gmail.com>
2019-02-12 12:11:51 +0100

    Solve scrabble score

--- scrabble-score/src/Scrabble.hs
+++ scrabble-score/src/Scrabble.hs
@@ -1,7 +1,20 @@
 module Scrabble (scoreLetter, scoreWord) where
 
+import           Data.Char
+import           Data.Map  (fromList, (!))
+
 scoreLetter :: Char -> Integer
-scoreLetter letter = error "You need to implement this function."
+scoreLetter letter
+    | isAlpha letter = fromList letterValue ! toLower letter
+    | otherwise = 0
+    where letterValue = [('a', 1), ('e', 1), ('i', 1), ('o', 1), ('u', 1),
+                         ('l', 1), ('n', 1), ('r', 1), ('s', 1), ('t', 1),
+                         ('d', 2), ('g', 2),
+                         ('b', 3), ('c', 3), ('m', 3), ('p', 3),
+                         ('f', 4), ('h', 4), ('v', 4), ('w', 4), ('y', 4),
+                         ('k', 5),
+                         ('j', 8), ('x', 8),
+                         ('q', 10), ('z', 10)]
 
 scoreWord :: String -> Integer
-scoreWord word = error "You need to implement this function."
+scoreWord = sum . map scoreLetter
