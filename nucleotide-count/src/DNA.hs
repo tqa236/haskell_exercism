01462b750a862b690406051cb9ea4854f9fc700f
TQA <trinhquocanh94@gmail.com>
2019-02-14 10:50:52 +0100

    Solve nucleotide count

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE TupleSections #-}
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import           Data.Either.Combinators (mapBoth)
-import           Data.List
 import           Data.Map                (Map, fromListWith)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded)
@@ -10,17 +10,15 @@ nucs :: [Nucleotide]
 nucs = [minBound..maxBound] -- [minBound..] works too, so does [A..] or [A..T]
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = countAll
-    where countAll = fromListWith (+)
-                      -- . mapBoth (\x -> x) (\x -> (head x, length x))
-                      . map (\x -> (head x, length x))
-                      . group
-                      . sort
-                      <$> traverse convertStrand xs
+nucleotideCounts xs = fromListWith (+) <$> countAll
+    where countAll = (map (, 0) nucs ++) <$> mapM convertStrand xs
 
-convertStrand :: Char -> Either String Nucleotide
-convertStrand c = case c of 'A' -> Right A
-                            'C' -> Right C
-                            'G' -> Right G
-                            'T' -> Right T
-                            _   -> Left "Invalid strand"
+convertStrand :: Char -> Either String (Nucleotide, Int)
+convertStrand c = mapBoth (const "Invalid strand") (, 1) (readEither c)
+
+readEither :: Char -> Either String Nucleotide
+readEither c = case c of 'A' -> Right A
+                         'C' -> Right C
+                         'G' -> Right G
+                         'T' -> Right T
+                         _   -> Left "Invalid strand"
