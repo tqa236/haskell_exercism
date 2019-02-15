c5afa2000e8964c789d61353229dd4f864b27368
TQA <trinhquocanh94@gmail.com>
2019-02-15 15:52:17 +0100

    Solve grains

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -1,7 +1,9 @@
 module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
-square = Just 2 (^^)
+square n
+    | n> 0 && n <= 64 = Just (2 ^ fromIntegral(n - 1))
+    | otherwise = Nothing
 
 total :: Integer
 total = 2 ^ 64 - 1
--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -3,22 +3,16 @@ module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import           Data.Either.Combinators (mapBoth)
 import           Data.Map                (Map, fromListWith)
+import           Text.Read               (readEither)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Enum, Bounded, Read)
 
 nucs :: [Nucleotide]
 nucs = [minBound..maxBound] -- [minBound..] works too, so does [A..] or [A..T]
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Integer)
-nucleotideCounts xs = fromListWith (+) <$> countAll
-    where countAll = (map (, 0) nucs ++) <$> mapM convertStrand xs
+nucleotideCounts xs = fromListWith (+) . (map (, 0) nucs ++)
+                        <$> mapM convertStrand xs
 
 convertStrand :: Char -> Either String (Nucleotide, Integer)
-convertStrand c = mapBoth (const "Invalid strand") (, 1) (readEither c)
-
-readEither :: Char -> Either String Nucleotide
-readEither c = case c of 'A' -> Right A
-                         'C' -> Right C
-                         'G' -> Right G
-                         'T' -> Right T
-                         _   -> Left "Invalid strand"
+convertStrand c = mapBoth (const "Invalid strand") (, 1) (readEither [c])
