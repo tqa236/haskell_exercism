17e308e94bf261a9eabe89ddefde131ee9527a67
TQA <trinhquocanh94@gmail.com>
2019-02-16 19:37:34 +0100

    Solve raindrops

--- grade-school/src/School.hs
+++ grade-school/src/School.hs
@@ -1,15 +1,13 @@
 module School (School, add, empty, grade, sorted) where
 
 import           Data.List
-import           Data.Map        (Map)
-import qualified Data.Map        as Map
-import           Data.SortedList (SortedList)
-import qualified Data.SortedList as SortedList
+import           Data.Map  (Map)
+import qualified Data.Map  as Map
 
 type School = Map Int [String]
 
 add :: Int -> String -> School -> School
-add gradeNum student = Map.insertWith (++) gradeNum [student]
+add gradeNum student = Map.fromList [gradeNum, [student]]
 
 empty :: School
 empty = Map.empty
@@ -18,4 +16,4 @@ grade :: Int -> School -> [String]
 grade gradeNum school = error "You need to implement this function."
 
 sorted :: School -> [(Int, [String])]
-sorted = sort . map (fmap SortedList.fromSortedList) . Map.toList
+sorted = Map.toList
--- raindrops/src/Raindrops.hs
+++ raindrops/src/Raindrops.hs
@@ -1,4 +1,11 @@
 module Raindrops (convert) where
 
+import           Data.Map (fromList, (!))
+
 convert :: Int -> String
-convert n = error "You need to implement this function."
+convert n | null string = show n
+          | otherwise   = string
+    where string                  = concatMap (rain n) [3, 5, 7]
+          rain x y | mod x y == 0 = raindrops ! y
+                   | otherwise    = ""
+          raindrops = fromList [(3, "Pling"), (5, "Plang"), (7, "Plong")]
