353c41f0b49cc43b30c66fa8c474318c237f30c3
TQA <trinhquocanh94@gmail.com>
2019-02-18 19:27:14 +0100

    Solve some exercises

--- kindergarten-garden/src/Garden.hs
+++ kindergarten-garden/src/Garden.hs
@@ -4,16 +4,28 @@ module Garden
     , lookupPlants
     ) where
 
+import qualified Data.Map as Map
+
 data Plant = Clover
            | Grass
            | Radishes
            | Violets
            deriving (Eq, Show)
 
-data Garden = Dummy
+type Garden = Map.Map String [Plant]
+
+toPlant :: Char -> Plant
+toPlant 'C' = Clover
+toPlant 'G' = Grass
+toPlant 'R' = Radishes
+toPlant _   = Violets
 
 garden :: [String] -> String -> Garden
-garden students plants = error "You need to implement this function."
+garden students plants =
+  let ss = concatMap (\x -> [x, x]) students
+      ps = map (map toPlant) $ lines plants
+      xs = concatMap (zip ss) ps
+  in foldr (\(k,v) -> Map.insertWith (++) k [v]) Map.empty xs
 
 lookupPlants :: String -> Garden -> [Plant]
-lookupPlants student garden = error "You need to implement this function."
+lookupPlants = Map.findWithDefault []
--- largest-series-product/src/Series.hs
+++ largest-series-product/src/Series.hs
@@ -1,6 +1,16 @@
 module Series (Error(..), largestProduct) where
 
+import           Data.Char (digitToInt)
+
 data Error = InvalidSpan | InvalidDigit Char deriving (Show, Eq)
 
 largestProduct :: Int -> String -> Either Error Integer
-largestProduct size digits = error "You need to implement this function."
+largestProduct size digits
+    | size > length digits || size < 0 = Left InvalidSpan
+    | all (`elem` ['0'..'9']) digits = Right $ toInteger $ maximum $ map product series
+    | otherwise = Left (InvalidDigit x)
+    where series = slices size digits
+          x = head $ filter (not . (`elem` ['0'..'9'])) digits
+
+slices :: Int -> String -> [[Int]]
+slices n xs = fmap (\i -> (fmap digitToInt. take n. drop i) xs) [0..length xs - n]
