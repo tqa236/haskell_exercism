912d0d3bad6c5de1a7525dd860cc171fe49e3279
TQA <trinhquocanh94@gmail.com>
2019-11-10 20:52:57 +0100

    Update Travis CI config

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+for path in */; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    # [ "${dirname}" != "coverage" ] || continue # Use for covedev
+    cd "$dirname" || exit
+    echo "$dirname"
+    stack test
+    cd ..
+done
